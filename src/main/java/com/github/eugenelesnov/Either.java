9d8f2b6225d2ed6998a0bd9a9db0ace0fe863192
EugeneLesnov <zheka.lesnov1996@gmail.com>
2021-03-09 07:40:11 +0300

    chore: added javadoc for Either class

--- src/main/java/com/github/eugenelesnov/Either.java
+++ src/main/java/com/github/eugenelesnov/Either.java
@@ -8,6 +8,13 @@ import java.util.function.Supplier;
  */
 public abstract class Either<L, R> {
 
+    /**
+     * Left handed implementation of `Either`
+     *
+     * @param leftSupplier  - supplier for left value
+     * @param rightSupplier - supplier for right value
+     * @return {@link Left} or {@link Right} implementation
+     */
     public static <L, R> Either<L, R> either(Supplier<L> leftSupplier, Supplier<R> rightSupplier) {
         L leftValue = leftSupplier.get();
         if (leftValue != null) {
@@ -17,7 +24,17 @@ public abstract class Either<L, R> {
         }
     }
 
+    /**
+     * Getter with optional for left value
+     *
+     * @return optional of left value
+     */
     public abstract Optional<L> getLeft();
 
+    /**
+     * Getter with optional for right value
+     *
+     * @return optional of right value
+     */
     public abstract Optional<R> getRight();
 }
